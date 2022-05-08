89a1e8a80b8abf604221daac4cd55309ce62c7fc
mickela <osoba.michael@outlook.com>
2022-05-09 00:10:18 +0100

    Update footer quote

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -41,7 +41,7 @@ const Footer = ({ footerSolidBg }) => {
                     <img src="assets/img/logo2-trans.png" alt="Hubswitch" />
                   </div>
                   <p>
-                    Changing Africa. Five lives a time
+                    A haven for beautiful minds ⚡
                   </p>
                   {/* <div className="newsletter-form">
                     <h5 className="form-title">Join Newsletters</h5>
