9bf4c3e0df71e221c69e89f688f447e566969407
mickela <osoba.michael@outlook.com>
2022-06-06 13:37:01 +0100

    Embed google forms

--- pages/apply.js
+++ pages/apply.js
@@ -19,169 +19,12 @@ const Contact = () => {
         <div className="container">
         
           {/* Contact Info End */}
-          <div className="apply-from-area mt-1">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScsqWPI7sV5s6adHE0rf4aODmEK-gpUZ-gLp7lxoV5WUpU0eQ/viewform?embedded=true" width="100%" height="2100" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+          {/* <div className="apply-from-area mt-1">
             <div className="row no-gutters">
-            
-              <div className="col-lg-12">
-                <div className="apply-form">
-                  <form 
-                  name="contact"
-                  // onSubmit={(e) => e.preventDefault()} 
-                  method="POST" action="/success" data-netlify="true">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <h3 className="form-title">Tell us about Yourself</h3>
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <div className="form-field mb-25">
-                          <label htmlFor="name">Full Name</label>
-                          <input
-                            type="text"
-                            placeholder="John Doe"
-                            id="name" name="name"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="form-field mb-25">
-                          <label htmlFor="phone-number">Phone Number</label>
-                          <input
-                            type="number"
-                            placeholder=""
-                            id="phone-number"
-                            name="number"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="form-field mb-25">
-                          <label htmlFor="email">Email</label>
-                          <input
-                            type="email"
-                            placeholder="john@example.com"
-                            id="email"
-                            name="name"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="form-field mb-25">
-                          <label htmlFor="subject">Country of Origin</label>
-                          <select name="country" required>
-                             <option> Nigeria</option>
-                             <option> Ethiopia</option>
-                             <option> Democratic Republic of the Congo</option>
-                             <option> Egypt</option>
-                             <option> South Africa</option>
-                             <option> Tanzania</option>
-                             <option> Kenya</option>
-                             <option> Algeria</option>
-                             <option> Uganda</option>
-                             <option> Sudan</option>
-                             <option> Morocco</option>
-                             <option> Angola</option>
-                             <option> Ghana</option>
-                             <option> Cameroon</option>
-                             <option> Madagascar</option>
-                             <option> Mozambique</option>
-                             <option> Ivory Coast</option>
-                             <option> Niger</option>
-                             <option> Mali</option>
-                             <option> Malawi</option>
-                             <option> Burkina Faso</option>
-                             <option> Chad</option>
-                             <option> Somalia 	</option>
-                             <option> Zimbabwe</option>
-                             <option> Zambia</option>
-                             <option> Senegal</option>
-                             <option> South Sudan</option>
-                             <option> Rwanda</option>
-                             <option> Guinea</option>
-                             <option> Benin</option>
-                             <option> Tunisia</option>
-                             <option> Burundi</option>
-                             <option> Sierra Leone</option>
-                             <option> Togo</option>
-                             <option> Libya</option>
-                             <option> Republic of the Congo</option>
-                             <option> Liberia</option>
-                             <option> Central African Republic</option>
-                             <option> Mauritania</option>
-                             <option> Eritrea</option>
-                             <option> Namibia</option>
-                             <option> Gambia </option>
-                             <option> Botswana</option>
-                             <option> Gabon</option>
-                             <option> Lesotho</option>
-                             <option> Guinea-Bissau</option>
-                             <option> Equatorial Guinea</option>
-                             <option> Mauritius</option>
-                             <option> Eswatini</option>
-                             <option> Djibouti</option>
-                             <option> Réunion (France)</option>
-                             <option> Comoros</option>
-                             <option> Cape Verde</option>
-                             <option> Western Sahara</option>
-                             <option> Mayotte (France)</option>
-                             <option> São Tomé and Príncipe</option>
-                             <option> Seychelles</option>
-                             <option> Saint Helena, Ascension and Tristan da Cunha (UK) </option>
-                            
 
-                          </select>
-                        </div>
-                      </div>
-
-
-                      <div className="col-lg-6">
-                        <div className="form-field mb-25">
-                          <label htmlFor="subject">Are you currently employed?</label>
-                          <select name="employed" required>
-                          <option>No</option>
-                              <option>Yes</option>
-                          </select>
-                        </div>
-                      </div>
-
-                      <div className="col-lg-6">
-                        <div className="form-field mb-25">
-                          <label htmlFor="subject">What's your preferred track?</label>
-                          <select name="track" required>
-                          <option>Web</option>
-                              <option>Mobile</option>
-                          </select>
-                        </div>
-                      </div>
-
-
-
-
-                      <div className="col-12">
-                        <div className="form-field mb-30">
-                          <label htmlFor="message">Tell us why you should be selected</label>
-                          <textarea
-                            id="message"
-                            placeholder="Hello"
-                            defaultValue={""}
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="col-12">
-                        <div className="form-field">
-                          <button className="main-btn" type="submit">
-                            Apply <i className="far fa-arrow-right" />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
             </div>
-          </div>
+          </div> */}
 
 
 
@@ -221,7 +64,7 @@ const Contact = () => {
                       </div>
                       <div className="info-content">
                         <h5>Our Location</h5>
-                        <p>Metaverse</p>
+                        <p>4, Raji Aliu Street, Ajao Estate, Isolo Lagos</p>
                       </div>
                     </div>
                     <div
